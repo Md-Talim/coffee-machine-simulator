557f3173bcbb14ca334d7284fda8e008b4ad4e51
Mohammad Talim <talimmohammad116@gmail.com>
2025-04-27 03:45:51 +0000

    Calculate ingredients based on user input

--- src/App.java
+++ src/App.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write how many cups of coffee you will need: ");
+
+        int cups = scanner.nextInt();
+        int water = 200 * cups;
+        int milk = 50 * cups;
+        int coffeeBeans = 15 * cups;
+
+        System.out.printf("For %d cups of coffee you will need:\n", cups);
+        System.out.printf("%d ml of water\n", water);
+        System.out.printf("%d ml of milk\n", milk);
+        System.out.printf("%d g of coffee beans\n", coffeeBeans);
+
+        scanner.close();
     }
 }
